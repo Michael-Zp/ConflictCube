e142d3ad89a25225a8fbee0c555b72a9bfca3db4
MichaelZp <michael.zappe@yahoo.de>
2017-10-31 12:16:09 +0100

    Initialized Project

---
+++ ConflictCubeTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConflictCubeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
