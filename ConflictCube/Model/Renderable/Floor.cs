a837c59c78085b015577d3565baae78300e9e101
MichaelZp <michael.zappe@yahoo.de>
2017-11-07 09:32:01 +0100

    Hups changed a file on develop. No functionality changed

--- ConflictCube/Model/Renderable/Floor.cs
+++ ConflictCube/Model/Renderable/Floor.cs
@@ -3,9 +3,6 @@ using OpenTK;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Zenseless.Geometry;
 
 namespace ConflictCube.Model.Renderable
