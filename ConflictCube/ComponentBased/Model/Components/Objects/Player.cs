1abb4ac45fb026e0b9f864aaadcacd538abed578
MichaelZp <michael.zappe@yahoo.de>
2017-12-16 13:10:47 +0100

    Added all necessary code. Need changes from feature/SetCubeDifferentBeha... to work (OnRemove method in component). In develop this feature is already added, but should work

--- ConflictCube/ComponentBased/Model/Components/Objects/Player.cs
+++ ConflictCube/ComponentBased/Model/Components/Objects/Player.cs
@@ -3,7 +3,6 @@ using System;
 using ConflictCube.ComponentBased.Components;
 using System.Collections.Generic;
 using ConflictCube.ComponentBased.Model.Components.Objects;
-using ConflictCube.GlobalMethods;
 
 namespace ConflictCube.ComponentBased
 {
@@ -13,6 +12,7 @@ namespace ConflictCube.ComponentBased
         public bool IsAlive { get; private set; }
         public bool ThrowMode { get; set; } = false;
         public bool UseMode { get; set; } = false;
+        public bool SledgeHammerMode { get; set; } = false;
         public float MaxSprintEnergy = 100;
         public float CurrentSprintEnergy = 100;
         public float UsedSprintEnergyPerSecond = 100;
@@ -31,6 +31,7 @@ namespace ConflictCube.ComponentBased
         private static bool MaterialsAreInitialized = false;
         private static Material UseMaterial;
         private static Material ThrowMaterial;
+        private static Material SledgeHammerMaterial;
         private float ThrowUseXOffset = 0;
         private float ThrowUseYOffset = 0;
 
@@ -66,6 +67,7 @@ namespace ConflictCube.ComponentBased
                 MaterialsAreInitialized = true;
                 UseMaterial = new Material(null, null, System.Drawing.Color.FromArgb(128, 255, 0, 0));
                 ThrowMaterial = new Material(null, null, System.Drawing.Color.FromArgb(128, 0, 0, 255));
+                SledgeHammerMaterial = new Material(null, null, System.Drawing.Color.FromArgb(128, 0, 255, 0));
             }
 
             ThrowUseField = new ColoredBox("ThrowUseIndicator", new Transform(), UseMaterial, this, false);
@@ -130,16 +132,16 @@ namespace ConflictCube.ComponentBased
 
             if (Input.OnButtonDown(Use, ActiveGamePad))
             {
-                ThrowOrUseBlock();
+                UseCurrentSelectedItem();
             }
 
             Move(moveVector);
             UpdateThrowUseField();
         }
 
-        private void ThrowOrUseBlock()
+        private void UseCurrentSelectedItem()
         {
-            if (UseMode || ThrowMode)
+            if (UseMode || ThrowMode || SledgeHammerMode)
             {
                 Vector2 currentPosOfThrowUseField = Floors[CurrentFloor].GetGridPosition(Transform.TransformToGlobal()) + new Vector2(ThrowUseXOffset, ThrowUseYOffset);
 
@@ -163,12 +165,16 @@ namespace ConflictCube.ComponentBased
                     }
                     Inventory.Cubes -= 1;
                 }
+                else if (SledgeHammerMode)
+                {
+                    Floors[CurrentFloor].FloorTiles[indexOfRow, indexOfColumn].HitFloorTileWithSledgeHammer();
+                }
             }
         }
 
         private void UpdateThrowUseField()
         {
-            if (Time.Time.CooldownIsOver(LastThrowUseFieldUpdate, ThrowUseFieldUpdateCooldown) && ThrowMode || UseMode)
+            if (Time.Time.CooldownIsOver(LastThrowUseFieldUpdate, ThrowUseFieldUpdateCooldown) && ThrowMode || UseMode || SledgeHammerMode)
             {
                 LastThrowUseFieldUpdate = Time.Time.CurrentTime;
 
@@ -272,7 +278,7 @@ namespace ConflictCube.ComponentBased
         /// </summary> 
         public void SwitchBetweenModes()
         {
-            if (!ThrowMode && !UseMode)
+            if (!ThrowMode && !UseMode && !SledgeHammerMode)
             {
                 ThrowUseXOffset = 0;
                 ThrowUseYOffset = 0;
@@ -291,6 +297,13 @@ namespace ConflictCube.ComponentBased
             else if (ThrowMode)
             {
                 ThrowMode = false;
+                SledgeHammerMode = true;
+                ThrowUseField.RemoveComponent<Material>();
+                ThrowUseField.AddComponent(SledgeHammerMaterial);
+            }
+            else if (SledgeHammerMode)
+            {
+                SledgeHammerMode = false;
                 ThrowUseField.Enabled = false;
             }
         }
